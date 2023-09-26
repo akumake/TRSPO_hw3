7ee15880c15ff2a1e14c7a18f1fdd564d84a1e8b
AndriyBelchik <belchikand@gmail.com>
2023-09-26 14:16:19 +0300

    dev

---
+++ main.py
@@ -0,0 +1,29 @@
+import multiprocessing as mp
+import time
+
+def colatz_steps(col_num: int):
+    steps = 0
+    while col_num != 1:
+        if col_num % 2 == 0:
+            col_num = col_num // 2
+        else:
+            col_num = 3 * col_num + 1
+
+        steps += 1
+
+    return steps
+
+if __name__ == '__main__':
+    N = 1000000
+    num_threads = 14
+    numbers = list(range(1, N+1))
+
+    pool = mp.Pool(num_threads)
+
+    results = pool.map(colatz_steps, numbers)
+
+    pool.close()
+    pool.join()
+
+    average_steps = sum(results) / len(numbers)
+    print(f'Середня кількість кроків: {average_steps}')
